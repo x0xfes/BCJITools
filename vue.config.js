431e25828b879bcbe1285b7115a6bdc09e42d0a1
x0xfes <x0xfes@gmail.com>
2021-04-02 20:54:57 +0900

    ページタイトルを指定

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   publicPath: '/BCJITools/',
   outputDir: 'docs',
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: "BCJI Tools"
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
